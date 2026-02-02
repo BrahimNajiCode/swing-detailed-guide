85a8576627b5a252c5e1cae3cae06b44195526b8
Brahim Naji <ba3105947@gmail.com>
2026-02-02 12:18:57 +0100

    Layouts: GridLayout added.

--- src/Layouts/GridL.java
+++ src/Layouts/GridL.java
@@ -1,4 +1,28 @@
 package Layouts;
 
-public class GridL {
+
+import javax.swing.*;
+import java.awt.*;
+
+// Le gestionnaire de mise en forme GridLayout permet de
+// disposer les composants les uns à la suite des autres sur une
+// grille régulière, chaque composant occupant une cellule de la
+// grille.
+public class GridL extends JFrame {
+    private JButton[] buttons;
+    public GridL(){
+        super("GridLayout Demo");
+        setSize(500,400);
+        // public GridLayout(int rows, int cols) ;
+        // public GridLayout(int rows, int cols, int hgap, int vgap) ;
+        setLayout(new GridLayout(3,3,5,5));
+        buttons = new JButton[9];
+        for (int i=0;i<buttons.length;++i){
+            buttons[i]= new JButton("Button "+(i+1));
+            add(buttons[i]);
+        }
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
--- src/Layouts/Main.java
+++ src/Layouts/Main.java
@@ -3,7 +3,7 @@ package Layouts;
 public class Main {
     public static void main(String[] args) {
         // Le gestionnaire BorderLayout est le gestionnaire par défaut des fenêtres et des boîtes de dialogue
-        new CardL();
+        new GridL();
     }
 }
 
